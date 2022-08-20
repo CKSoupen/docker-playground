36aa6a725ec75bfb8f532b556d8f5c50a08cfd76
CKS2 <cameron@interviewme.tech>
2022-08-20 18:05:23 +0200

    Refactor folder name to keep with standards of naming

--- 1_fastapi-uvicorn/api/main.py
+++ 1_fastapi-uvicorn/api/main.py
@@ -11,5 +11,5 @@ def read_root():
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+def read_item(item_id: int, message: Union[str, int] = 52):
+    return {"item_id": item_id, "message": message}
